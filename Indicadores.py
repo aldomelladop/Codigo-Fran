a8123a11f74ab17ba7907e61ec2e855315a3e35b
aldomelladop <aldomellado.1310@gmail.com>
2019-12-18 19:10:40 -0300

    mensaje

--- Indicadores.py
+++ Indicadores.py
@@ -376,17 +376,23 @@ while(l<np.shape(tipo)[0]):
             if globals()['tabla_{}'.format(tipo[l])].iloc[i,0] == j:
                 # globals()['tabla_{}'.format(tipo[l])].iloc[i,3] = str(round(float(globals()['tabla_{}'.format(tipo[l])].iloc[i,3])/tiempos[tipo[l]][str(j)],1)*100) +'%'
                 globals()['tabla_{}'.format(tipo[l])].iloc[i,3] = round(float(globals()['tabla_{}'.format(tipo[l])].iloc[i,3])/tiempos[tipo[l]][str(j)],1)
-    l+=1
     
-# =============================================================================
-# Grafico  
-# =============================================================================
+    # =============================================================================
+    # Grafico  
+    # =============================================================================
+                
+    line_chart = pygal.Bar()
+    line_chart.title = 'Eficiencia técnicos atenciones de tipo ' + tipo[l]
+    line_chart.x_labels = map(str, nom_tec)
+    
+    line_chart.add('Hola', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.render_to_file('line_chart_'+tipo[l]+'.svg')
+    line_chart.render_in_browser()
+    l+=1
 
-line_chart = pygal.Bar()
-line_chart.title = 'Browser usage evolution (in %)'
-line_chart.x_labels = map(str, range(2002, 2013))
-line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-line_chart.render_in_browser()
+# =============================================================================
+# Subir codigo
+# =============================================================================
+!git add .
+!git commit -m "mensaje"
+!git push
