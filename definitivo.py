9ce27a332178b2605e7703fff5202b1000611a36
aldomelladop <aldomellado.1310@gmail.com>
2020-02-03 22:42:51 -0300

    Actualizando repositorio

--- definitivo.py
+++ definitivo.py
@@ -322,7 +322,7 @@ data['angle'] = data['value']/sum(x_T1.values()) * 2*pi
 data['color'] = Category20c[len(x_T1)]
 
 s4 = figure(plot_height=350, title="* Cuarto Indicador: \t% de OT cerradas v/s total mes\n{}".format(fecha), toolbar_location=None,
-           tools="hover", tooltips=[("Cerradas", "@country"),("Value", "@value"*2)])
+           tools="hover", tooltips=[("Cerradas", "@country"),("Value", "2*@value")])
 
 s4.annular_wedge(x=0, y=1, inner_radius=0.2, outer_radius=0.4,
                 start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
